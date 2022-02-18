759730e9aca7a70e2334221fa7e3b14e188c70a8
linux_china <libing.chen@gmail.com>
2022-02-18 15:52:53 -0800

    JustFile PSI

---
+++ src/main/kotlin/org/mvnsearch/plugins/just/lang/psi/JustFile.kt
@@ -0,0 +1,75 @@
+package org.mvnsearch.plugins.just.lang.psi
+
+import com.intellij.extapi.psi.PsiFileBase
+import com.intellij.openapi.project.guessProjectDir
+import com.intellij.psi.FileViewProvider
+import com.intellij.psi.PsiManager
+import org.mvnsearch.plugins.just.lang.JustLanguage
+import org.mvnsearch.plugins.just.parseRecipeName
+
+class JustFile(viewProvider: FileViewProvider?) : PsiFileBase(viewProvider!!, JustLanguage) {
+    override fun getFileType() = JustFileType
+    override fun toString() = "Justfile"
+
+
+    fun findRecipeElement(recipe: String): JustRecipeStatement? {
+        return this.children
+            .filterIsInstance<JustRecipeStatement>()
+            .firstOrNull {
+                val recipeName = it.recipeName.text
+                recipeName == recipe || recipeName == "@${recipe}"
+            }
+    }
+
+    fun parseMetadata(loadDotenv: Boolean): JustfileMetadata {
+        val justfileMetadata = JustfileMetadata()
+        text.lines().forEach { line ->
+            if (line.startsWith("export ") && line.contains(":=")) { //export
+                val variable = line.substring(7, line.indexOf(":=")).trim()
+                justfileMetadata.envVariables.add(variable)
+            } else if (line.startsWith("set ")) { //set
+                if (line.contains("dotenv-load")) {
+                    justfileMetadata.dotenvLoad = line.contains("dotenv-load") && !line.contains("false")
+                } else if (line.contains("export")) {
+                    justfileMetadata.variablesExported = !line.contains("false")
+                }
+            } else if (line.startsWith("alias ") && line.contains(":=")) {
+                val aliasRecipeName = line.substring(6, line.indexOf(":=")).trim()
+                justfileMetadata.recipes.add(parseRecipeName(aliasRecipeName))
+            } else if (!(line.startsWith(" ") || line.startsWith("\t")) && line.contains(":")) { //recipe or variable
+                if (line.contains(":=")) { // variable
+                    val variable = line.substring(0, line.indexOf(":=")).trim()
+                    justfileMetadata.variables.add(variable)
+                } else { // recipe
+                    justfileMetadata.recipes.add(parseRecipeName(line))
+                }
+            }
+        }
+        if (justfileMetadata.variablesExported) {
+            justfileMetadata.envVariables.addAll(justfileMetadata.variables)
+            justfileMetadata.variables.clear()
+        }
+        if (loadDotenv && justfileMetadata.dotenvLoad) {
+            val projectDir = project.guessProjectDir()
+            projectDir?.findChild(".env")?.let {
+                val envFile = PsiManager.getInstance(project).findFile(projectDir.findChild(".env")!!)!!
+                envFile.text.lines().forEach { line ->
+                    if (!line.startsWith("#") && line.contains("=")) {
+                        val variable = line.substring(0, line.indexOf('='))
+                        justfileMetadata.envVariables.add(variable)
+                    }
+                }
+            }
+        }
+        return justfileMetadata
+    }
+}
+
+
+class JustfileMetadata {
+    val recipes = mutableListOf<String>()
+    val variables = mutableListOf<String>()
+    val envVariables = mutableListOf<String>()
+    var dotenvLoad = false
+    var variablesExported = false
+}
