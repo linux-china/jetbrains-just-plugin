6db3be55b845bd70133d3b3f6e06d452259c3cce
linux_china <libing.chen@gmail.com>
2022-02-18 15:45:05 -0800

    JustIcons

---
+++ src/main/kotlin/org/mvnsearch/plugins/just/ide/icons/JustIcons.kt
@@ -0,0 +1,7 @@
+package org.mvnsearch.plugins.just.ide.icons
+
+import com.intellij.openapi.util.IconLoader
+
+object JustIcons {
+    val JUST_FILE = IconLoader.findIcon("/icons/just-16x16.svg")!!
+}
