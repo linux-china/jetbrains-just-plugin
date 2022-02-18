4f3bfaadcec7c3fc22e7b99fefd453e15b141a91
linux_china <libing.chen@gmail.com>
2022-02-18 15:47:20 -0800

    JustCodeBlockLanguageInjector

---
+++ src/main/kotlin/org/mvnsearch/plugins/just/lang/injector/JustCodeBlockLanguageInjector.kt
@@ -0,0 +1,29 @@
+package org.mvnsearch.plugins.just.lang.injector
+
+import com.intellij.lang.Language
+import com.intellij.lang.injection.general.Injection
+import com.intellij.lang.injection.general.LanguageInjectionContributor
+import com.intellij.lang.injection.general.SimpleInjection
+import com.intellij.psi.PsiElement
+import org.mvnsearch.plugins.just.lang.psi.JustCodeBlock
+
+
+class JustCodeBlockLanguageInjector : LanguageInjectionContributor {
+    var shellLanguage: Language? = null
+
+    init {
+        shellLanguage = Language.findLanguageByID("Shell Script");
+        if (shellLanguage == null) {
+            shellLanguage = Language.findLanguageByID("BashPro Shell Script")
+        }
+    }
+
+    override fun getInjection(context: PsiElement): Injection? {
+        if (context is JustCodeBlock && shellLanguage != null) {
+            return SimpleInjection(shellLanguage!!, "", "", null)
+        }
+        return null
+    }
+
+
+}
