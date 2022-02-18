825276b0d6532f79dcd7e364ef906c478eff4362
linux_china <libing.chen@gmail.com>
2022-02-18 15:46:48 -0800

    JustRecipeDependencyCompletionContributor

---
+++ src/main/kotlin/org/mvnsearch/plugins/just/lang/completion/JustRecipeDependencyCompletionContributor.kt
@@ -0,0 +1,46 @@
+package org.mvnsearch.plugins.just.lang.completion
+
+import com.intellij.codeInsight.completion.*
+import com.intellij.codeInsight.lookup.LookupElementBuilder
+import com.intellij.openapi.util.TextRange
+import com.intellij.patterns.PlatformPatterns.psiElement
+import com.intellij.psi.PsiElement
+import com.intellij.util.ProcessingContext
+import org.mvnsearch.plugins.just.lang.JustLanguage
+import org.mvnsearch.plugins.just.lang.psi.JustFile
+import org.mvnsearch.plugins.just.lang.psi.JustTypes
+import org.mvnsearch.plugins.just.parseRecipeName
+
+class JustRecipeDependencyCompletionContributor : CompletionContributor() {
+    init {
+        extend(
+            CompletionType.BASIC,
+            psiElement(PsiElement::class.java).withParent(psiElement(JustTypes.DEPENDENCIES_WITH_CODE)).withElementType(JustTypes.RECIPE_NAME).withLanguage(JustLanguage),
+            object : CompletionProvider<CompletionParameters>() {
+                override fun addCompletions(
+                    parameters: CompletionParameters,
+                    context: ProcessingContext,
+                    result: CompletionResultSet
+                ) {
+                    val element = parameters.position
+                    val caret = parameters.editor.caretModel.currentCaret
+                    val lineOffset = caret.visualLineStart
+                    val prefixText = parameters.editor.document.getText(TextRange(lineOffset, caret.offset))
+                    if (prefixText.contains(':')) {  //recipe names completion
+                        val excludedNames = mutableListOf(parseRecipeName(prefixText))
+                        val line = parameters.editor.document.getText(TextRange(lineOffset, caret.visualLineEnd))
+                        val dependencies = line.substring(line.indexOf(':') + 1).trim()
+                        if (dependencies.isNotEmpty()) {
+                            excludedNames.addAll(dependencies.split("\\s+".toRegex()))
+                        }
+                        val justfile = element.containingFile as JustFile
+                        val justfileMetadata = justfile.parseMetadata(false)
+                        justfileMetadata.recipes.filter { !excludedNames.contains(it) }.forEach {
+                            result.addElement(LookupElementBuilder.create(it))
+                        }
+                    }
+                }
+            }
+        )
+    }
+}
