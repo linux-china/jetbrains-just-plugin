653be2f813dc43c2792b241bfbac458fbc29d545
linux_china <libing.chen@gmail.com>
2022-02-19 13:40:06 -0800

    Add classloader

--- src/main/kotlin/org/mvnsearch/plugins/just/ide/icons/JustIcons.kt
+++ src/main/kotlin/org/mvnsearch/plugins/just/ide/icons/JustIcons.kt
@@ -3,5 +3,5 @@ package org.mvnsearch.plugins.just.ide.icons
 import com.intellij.openapi.util.IconLoader
 
 object JustIcons {
-    val JUST_FILE = IconLoader.findIcon("/icons/just-16x16.svg")!!
+    val JUST_FILE = IconLoader.findIcon("/icons/just-16x16.svg", JustIcons::class.java.classLoader)!!
 }
