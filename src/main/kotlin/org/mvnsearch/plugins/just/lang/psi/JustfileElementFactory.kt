882e83aff2b853619fcd86b044a4a80478d75049
linux_china <libing.chen@gmail.com>
2022-02-20 19:11:24 -0800

    JustfileElementFactory

---
+++ src/main/kotlin/org/mvnsearch/plugins/just/lang/psi/JustfileElementFactory.kt
@@ -0,0 +1,17 @@
+package org.mvnsearch.plugins.just.lang.psi
+
+import com.intellij.openapi.project.Project
+import com.intellij.psi.PsiFileFactory
+
+
+object JustfileElementFactory {
+    fun createFile(project: Project, text: String) =
+        PsiFileFactory.getInstance(project).createFileFromText("justfile", JustFileType, text) as JustFile
+
+
+    fun createCodeBlock(project: Project, codeBlock: String): JustCodeBlock {
+        val recipeStatement = createFile(project, "name:\n  $codeBlock").firstChild as JustRecipeStatement
+        return recipeStatement.dependenciesWithCode?.codeBlock!!
+    }
+
+}
