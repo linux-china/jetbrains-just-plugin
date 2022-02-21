88721b7c6a4c59a1d1cac7917cd65c7acc3c8dad
linux_china <libing.chen@gmail.com>
2022-02-20 19:06:55 -0800

    JustCodeBlockLanguageInjector

---
+++ src/main/kotlin/org/mvnsearch/plugins/just/lang/injector/JustCodeBlockLanguageInjector.kt
@@ -0,0 +1,43 @@
+package org.mvnsearch.plugins.just.lang.injector
+
+import com.intellij.lang.Language
+import com.intellij.lang.injection.MultiHostInjector
+import com.intellij.lang.injection.MultiHostRegistrar
+import com.intellij.openapi.project.DumbAware
+import com.intellij.openapi.util.TextRange
+import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiLanguageInjectionHost
+import org.mvnsearch.plugins.just.INDENT_CHARS
+import org.mvnsearch.plugins.just.PARAM_PREFIX_LIST
+import org.mvnsearch.plugins.just.lang.psi.JustCodeBlock
+
+
+class JustCodeBlockLanguageInjector : MultiHostInjector, DumbAware {
+    var shellLanguage: Language? = null
+
+    init {
+        shellLanguage = Language.findLanguageByID("Shell Script");
+        if (shellLanguage == null) {
+            shellLanguage = Language.findLanguageByID("BashPro Shell Script")
+        }
+    }
+
+
+    override fun getLanguagesToInject(registrar: MultiHostRegistrar, context: PsiElement) {
+        val text = context.text
+        if (!text.contains("#!/usr/bin/env") || text.contains("#!/usr/bin/env sh")) {
+            val offset = text.indexOfFirst { !INDENT_CHARS.contains(it) && !PARAM_PREFIX_LIST.contains(it) }
+            if (offset > 0) {
+                val injectionTextRange = TextRange(offset, context.textLength)
+                registrar.startInjecting(shellLanguage!!)
+                registrar.addPlace(null, null, context as PsiLanguageInjectionHost, injectionTextRange)
+                registrar.doneInjecting()
+            }
+        }
+    }
+
+    override fun elementsToInjectIn(): MutableList<out Class<out PsiElement>> {
+        return mutableListOf(JustCodeBlock::class.java)
+    }
+
+}
