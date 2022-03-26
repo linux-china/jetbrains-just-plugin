c1eb84d1a761dd15277698f18ffad9728a892c83
linux_china <libing.chen@gmail.com>
2022-03-26 16:25:04 -0700

    Just object

---
+++ src/main/kotlin/org/mvnsearch/plugins/just/Just.kt
@@ -0,0 +1,18 @@
+package org.mvnsearch.plugins.just
+
+import com.intellij.openapi.util.SystemInfo
+import java.io.File
+
+object Just {
+    fun getJustCmdAbsolutionPath(): String {
+        return if (SystemInfo.isWindows) {
+            return "just"
+        } else {
+            if (File("/usr/local/bin/just").exists()) {
+                "/usr/local/bin/just"
+            } else {
+                "just"
+            }
+        }
+    }
+}
