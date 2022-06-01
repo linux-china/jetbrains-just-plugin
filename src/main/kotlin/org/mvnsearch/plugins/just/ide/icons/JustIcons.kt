ed7083cd8482277871f2d3514f244f3aaec20665
linux_china <libing.chen@gmail.com>
2022-05-31 17:12:52 -0700

    chore: add JUST_FILE_12 icon

--- src/main/kotlin/org/mvnsearch/plugins/just/ide/icons/JustIcons.kt
+++ src/main/kotlin/org/mvnsearch/plugins/just/ide/icons/JustIcons.kt
@@ -4,4 +4,5 @@ import com.intellij.openapi.util.IconLoader
 
 object JustIcons {
     val JUST_FILE = IconLoader.findIcon("/icons/just-16x16.svg", JustIcons::class.java.classLoader)!!
+    val JUST_FILE_12 = IconLoader.findIcon("/icons/just-12x12.svg", JustIcons::class.java.classLoader)!!
 }
