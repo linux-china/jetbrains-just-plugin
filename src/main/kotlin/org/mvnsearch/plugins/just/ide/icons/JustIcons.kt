d21660c9864973ccbaf5527429a00558991d0094
linux_china <libing.chen@gmail.com>
2022-06-19 11:06:24 -0700

    feat: add more icons

--- src/main/kotlin/org/mvnsearch/plugins/just/ide/icons/JustIcons.kt
+++ src/main/kotlin/org/mvnsearch/plugins/just/ide/icons/JustIcons.kt
@@ -1,8 +1,12 @@
 package org.mvnsearch.plugins.just.ide.icons
 
+import com.intellij.icons.AllIcons
 import com.intellij.openapi.util.IconLoader
 
 object JustIcons {
     val JUST_FILE = IconLoader.findIcon("/icons/just-16x16.svg", JustIcons::class.java.classLoader)!!
     val JUST_FILE_12 = IconLoader.findIcon("/icons/just-12x12.svg", JustIcons::class.java.classLoader)!!
+    val RUN_ICON = AllIcons.RunConfigurations.TestState.Run
+    val VARIABLE_ICON = AllIcons.Nodes.Variable
+    var EXPORT_ICON = AllIcons.Nodes.Gvariable
 }
