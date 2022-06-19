249f4eefcdd33eb3d9b38b3786ba40aadb5601fb
linux_china <libing.chen@gmail.com>
2022-06-18 23:14:28 -0700

    feat: add JustRecipeElement

--- src/main/kotlin/org/mvnsearch/plugins/just/lang/psi/JustNamedElement.kt
+++ src/main/kotlin/org/mvnsearch/plugins/just/lang/psi/JustNamedElement.kt
@@ -1,19 +1,33 @@
 package org.mvnsearch.plugins.just.lang.psi
 
 import com.intellij.extapi.psi.ASTWrapperPsiElement
+import com.intellij.icons.AllIcons
 import com.intellij.lang.ASTNode
-import com.intellij.openapi.util.NlsSafe
+import com.intellij.navigation.ItemPresentation
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiNameIdentifierOwner
-import com.intellij.util.IncorrectOperationException
+import org.mvnsearch.plugins.just.lang.psi.JustfileElementFactory.createRecipe
+import javax.swing.Icon
 
-interface JustNamedElement : PsiNameIdentifierOwner {
+interface JustNamedElement : PsiNameIdentifierOwner
+
+interface JustRecipeElement : JustNamedElement {
+    fun getKey(): String?
+
+    fun getValue(): String?
+
+    override fun getName(): String?
+
+    override fun setName(name: String): PsiElement?
+
+    override fun getNameIdentifier(): PsiElement?
+
+    fun getPresentation(): ItemPresentation?
 }
-
 
 abstract class JustNamedElementImpl(node: ASTNode) : ASTWrapperPsiElement(node), JustNamedElement {
     private var _name: String? = null
-    
+
     override fun getName(): String? {
         return this._name
     }
@@ -22,9 +36,56 @@ abstract class JustNamedElementImpl(node: ASTNode) : ASTWrapperPsiElement(node),
         return this
     }
 
-    @Throws(IncorrectOperationException::class)
-    override fun setName(name: @NlsSafe String): PsiElement? {
-        this._name = name;
+    override fun setName(name: String): PsiElement? {
+        this._name = name
         return this
+    }
+}
+
+abstract class JustRecipeElementImpl(node: ASTNode) : JustNamedElementImpl(node), JustRecipeElement {
+    override fun getKey(): String? {
+        val keyNode: ASTNode? = this.node.findChildByType(JustTypes.RECIPE_NAME)
+        return keyNode?.text?.replace("\\\\ ".toRegex(), " ")
+    }
+
+    override fun getValue(): String? {
+        val valueNode: ASTNode? = this.node.findChildByType(JustTypes.RECIPE_NAME)
+        return valueNode?.text
+    }
+
+    override fun getName(): String? {
+        return getKey()
+    }
+
+    override fun setName(name: String): PsiElement? {
+        val keyNode: ASTNode? = this.node.findChildByType(JustTypes.RECIPE_NAME)
+        if (keyNode != null) {
+            val createdRecipe = createRecipe(this.project, name)
+            val newKeyNode = createdRecipe.firstChild.node
+            this.node.replaceChild(keyNode, newKeyNode)
+        }
+        return this
+    }
+
+    override fun getNameIdentifier(): PsiElement? {
+        return this.node.findChildByType(JustTypes.RECIPE_NAME)?.psi
+    }
+
+    override fun getPresentation(): ItemPresentation? {
+        val presentationText = this.getKey()
+        val element = this
+        return object : ItemPresentation {
+            override fun getPresentableText(): String? {
+                return presentationText
+            }
+
+            override fun getLocationString(): String? {
+                return element.containingFile?.name
+            }
+
+            override fun getIcon(unused: Boolean): Icon {
+                return AllIcons.RunConfigurations.TestState.Run
+            }
+        }
     }
 }
