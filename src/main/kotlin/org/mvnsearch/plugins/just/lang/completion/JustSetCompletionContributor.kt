3e968e54aff36c34980022f3c1554c01ff83ee45
Libing Chen <libing.chen@gmail.com>
2024-01-13 15:35:08 +0800

    chore: add more Settings

--- src/main/kotlin/org/mvnsearch/plugins/just/lang/completion/JustSetCompletionContributor.kt
+++ src/main/kotlin/org/mvnsearch/plugins/just/lang/completion/JustSetCompletionContributor.kt
@@ -25,13 +25,17 @@ class JustSetCompletionContributor : CompletionContributor() {
                     if (element.elementType == JustTypes.SETTING) {
                         result.addElement(LookupElementBuilder.create("allow-duplicate-recipes"))
                         result.addElement(LookupElementBuilder.create("dotenv-load := true").withPresentableText("dotenv-load"))
-                        result.addElement(LookupElementBuilder.create("fallback := true"))
+                        result.addElement(LookupElementBuilder.create("dotenv-filename := \"\""))
+                        result.addElement(LookupElementBuilder.create("dotenv-path := \"\""))
+                        result.addElement(LookupElementBuilder.create("ignore-comments"))
                         result.addElement(LookupElementBuilder.create("export"))
+                        result.addElement(LookupElementBuilder.create("quiet"))
+                        result.addElement(LookupElementBuilder.create("fallback"))
+                        result.addElement(LookupElementBuilder.create("tempdir := \"\""))
                         result.addElement(LookupElementBuilder.create("positional-arguments"))
                         result.addElement(LookupElementBuilder.create("shell := [\"bash\", \"-c\"]").withPresentableText("shell"))
                         result.addElement(LookupElementBuilder.create("windows-shell := [\"pwsh.exe\", \"-NoLogo\",\"-Command\"]").withPresentableText("shell"))
                         result.addElement(LookupElementBuilder.create("windows-powershell := true"))
-                        result.addElement(LookupElementBuilder.create("ignore-comments := true"))
                     }
                 }
             }
