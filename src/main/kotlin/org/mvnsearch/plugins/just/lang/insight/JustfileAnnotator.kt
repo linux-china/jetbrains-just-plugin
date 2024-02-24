867e73e7e98aa53643971884bf873b6e271c44d9
Libing Chen <libing.chen@gmail.com>
2024-02-24 11:44:19 +0800

    chore: high light for mod keyword

--- src/main/kotlin/org/mvnsearch/plugins/just/lang/insight/JustfileAnnotator.kt
+++ src/main/kotlin/org/mvnsearch/plugins/just/lang/insight/JustfileAnnotator.kt
@@ -4,8 +4,6 @@ import com.intellij.lang.annotation.AnnotationHolder
 import com.intellij.lang.annotation.Annotator
 import com.intellij.lang.annotation.HighlightSeverity
 import com.intellij.openapi.editor.DefaultLanguageHighlighterColors
-import com.intellij.openapi.editor.HighlighterColors
-import com.intellij.openapi.editor.colors.TextAttributesKey
 import com.intellij.openapi.util.TextRange
 import com.intellij.psi.PsiElement
 import com.intellij.psi.util.elementType
@@ -34,6 +32,7 @@ class JustfileAnnotator : Annotator {
                     .textAttributes(DefaultLanguageHighlighterColors.STATIC_FIELD).create()
             }
 
+            JustTypes.KEYWORD_MOD,
             JustTypes.KEYWORD_ALIAS,
             JustTypes.KEYWORD_SET,
             JustTypes.KEYWORD_EXPORT,
