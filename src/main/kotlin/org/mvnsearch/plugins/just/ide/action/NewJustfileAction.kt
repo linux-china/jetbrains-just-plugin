3c3402e1eec89c36e5ffde776797816d35d2ebf4
Libing Chen <libing.chen@gmail.com>
2024-10-08 15:30:37 +0800

    chore: add uv and zig project support

--- src/main/kotlin/org/mvnsearch/plugins/just/ide/action/NewJustfileAction.kt
+++ src/main/kotlin/org/mvnsearch/plugins/just/ide/action/NewJustfileAction.kt
@@ -10,6 +10,7 @@ import com.intellij.openapi.util.SystemInfo
 import com.intellij.psi.PsiDirectory
 import com.intellij.psi.PsiFile
 import java.io.File
+import java.nio.file.Files
 
 /**
  * create justfile by project type: maven, gradle or normal
@@ -33,71 +34,111 @@ class NewJustfileAction : AnAction() {
         updates:
           mvn versions:display-dependency-updates > updates.txt    
         """.trimIndent()
+        val JUSTFILE_MAVEN_SB = """
+        #!/usr/bin/env just --justfile
+        
+        # maven build without tests
+        build:
+           mvn -DskipTests clean package
+        
+        # start spring boot
+        start:
+           mvn -DskipTests spring-boot:run
+                   
+        # dependencies tree for compile
+        dependencies:
+          mvn dependency:tree -Dscope=compile > dependencies.txt
+        
+        # display updates
+        updates:
+          mvn versions:display-dependency-updates > updates.txt    
+        """.trimIndent()
         val JUSTFILE_GRADLE = """
-                #!/usr/bin/env just --justfile
-                
-                build:
-                  ./gradlew -x test build    
-                
-                dependencies:
-                  ./gradlew -q dependencies --configuration compileClasspath > ./dependencies.txt
-                
-                # Report up-to-date dependencies by com.github.ben-manes.versions
-                updates:
-                  ./gradlew dependencyUpdates > updates.txt
-                
-                wrapper:
-                  ./gradlew wrapper --gradle-version=7.6
-                """.trimIndent()
+        #!/usr/bin/env just --justfile
+        
+        build:
+          ./gradlew -x test build    
+        
+        dependencies:
+          ./gradlew -q dependencies --configuration compileClasspath > ./dependencies.txt
+        
+        # Report up-to-date dependencies by com.github.ben-manes.versions
+        updates:
+          ./gradlew dependencyUpdates > updates.txt
+        
+        wrapper:
+          ./gradlew wrapper --gradle-version=7.6
+        """.trimIndent()
         val JUSTFILE_CARGO = """
-                #!/usr/bin/env just --justfile
-                
-                release:
-                  cargo build --release    
-                
-                lint:
-                  cargo clippy
-                
-                bin:
-                  cargo run --bin bin -- arg1
-                
-                example:
-                  cargo run --example exname -- arg1
-                """.trimIndent()
+        #!/usr/bin/env just --justfile
+        
+        release:
+          cargo build --release    
+        
+        lint:
+          cargo clippy
+        
+        bin:
+          cargo run --bin bin -- arg1
+        
+        example:
+          cargo run --example exname -- arg1
+        """.trimIndent()
         val JUSTFILE_CMAKE = """
-                #!/usr/bin/env just --justfile
-                
-                release:
-                  cmake -S . -B build -D CMAKE_BUILD_TYPE=Release
-                  cmake --build build
-                """.trimIndent()
+        #!/usr/bin/env just --justfile
+        
+        release:
+          cmake -S . -B build -D CMAKE_BUILD_TYPE=Release
+          cmake --build build
+        """.trimIndent()
         val JUSTFILE_GOLANG = """
-                #!/usr/bin/env just --justfile
-                
-                update:
-                  go get -u
-                  go mod tidy -v
-                """.trimIndent()
+        #!/usr/bin/env just --justfile
+        
+        update:
+          go get -u
+          go mod tidy -v
+        """.trimIndent()
         val JUSTFILE_NODEJS = """
-                #!/usr/bin/env just --justfile
-                export PATH := "./node_modules/.bin:" + env_var('PATH')
-                
-                build:
-                  npm run build
-                """.trimIndent()
+        #!/usr/bin/env just --justfile
+        export PATH := justfile_directory() + "/node_modules/.bin:" + env_var('PATH')
+        
+        build:
+          npm run build
+        """.trimIndent()
+        val JUSTFILE_UV = """
+         #!/usr/bin/env just --justfile
+         export PATH := justfile_directory() + "/.venv/bin:" + env_var('PATH')
+         
+         upgrade:
+           uv lock --upgrade
+         """.trimIndent()
+        val JUSTFILE_ZIG = """
+         #!/usr/bin/env just --justfile
+         
+         build:
+           zig build--summary all
+         
+         run:
+           zig buildrun --summary all
+         """.trimIndent()
         val JUSTFILE = """
-                #!/usr/bin/env just --justfile
-                
-                hello:
-                  echo "hello world"
-                """.trimIndent()
+        #!/usr/bin/env just --justfile
+        
+        hello:
+          echo "hello world"
+        """.trimIndent()
     }
 
     override fun actionPerformed(e: AnActionEvent) {
         val project = e.getData(CommonDataKeys.PROJECT)!!
         val projectDir = project.guessProjectDir()!!
         val content = if (projectDir.findChild("pom.xml") != null) {
-            JUSTFILE_MAVEN
+            val pomXmlCode = Files.readString(projectDir.findChild("pom.xml")!!.toNioPath())
+            if (pomXmlCode.contains("spring-boot-starter")) {
+                JUSTFILE_MAVEN_SB
+            } else {
+                JUSTFILE_MAVEN
+            }
         } else if (projectDir.findChild("gradlew") != null) {
             JUSTFILE_GRADLE
         } else if (projectDir.findChild("Cargo.toml") != null) {
@@ -108,6 +149,10 @@ class NewJustfileAction : AnAction() {
             JUSTFILE_GOLANG
         } else if (projectDir.findChild("package.json") != null) {
             JUSTFILE_NODEJS
+        } else if (projectDir.findChild("uv.lock") != null) {
+            JUSTFILE_UV
+        } else if (projectDir.findChild("build.zig") != null) {
+            JUSTFILE_ZIG
         } else {
             JUSTFILE
         }
