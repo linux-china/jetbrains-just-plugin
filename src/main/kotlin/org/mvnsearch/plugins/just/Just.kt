5722295da0184d75428a180092d39b82149b398b
Libing Chen <libing.chen@gmail.com>
2025-09-10 09:40:08 +0800

    chore: polishing

--- src/main/kotlin/org/mvnsearch/plugins/just/Just.kt
+++ src/main/kotlin/org/mvnsearch/plugins/just/Just.kt
@@ -1,14 +1,30 @@
 package org.mvnsearch.plugins.just
 
+import com.intellij.openapi.application.PathMacros
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.guessProjectDir
 import com.intellij.openapi.util.SystemInfo
+import com.intellij.util.EnvironmentUtil
 import java.io.File
+import kotlin.io.path.Path
 
 object Just {
     fun getJustCmdAbsolutionPath(project: Project): String {
+        // environment variable
+        EnvironmentUtil.getValue("JUST_EXECUTABLE")?.let {
+            return it
+        }
+        // path macro
+        PathMacros.getInstance().getValue("JUST_EXECUTABLE")?.let {
+            return it
+        }
         project.guessProjectDir()?.let { projectDir ->
             projectDir.findFileByRelativePath(".devenv/profile/bin/just")?.let { return it.path }
+        }
+        // mise
+        val userHome = Path(System.getProperty("user.home"))
+        userHome.resolve(".local/share/mise/installs/just/latest/just").toFile().let { justExec ->
+            if (justExec.exists()) return justExec.absolutePath
         }
         return if (SystemInfo.isWindows) {
             return "just"
