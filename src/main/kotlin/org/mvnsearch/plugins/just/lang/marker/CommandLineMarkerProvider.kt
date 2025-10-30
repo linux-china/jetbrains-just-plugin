58d30778e6fb08f1a142515799e6a871732f515a
leijuan <libing.chen@gmail.com>
2025-10-30 14:02:54 +0800

    chore: adjust icon

--- src/main/kotlin/org/mvnsearch/plugins/just/lang/marker/CommandLineMarkerProvider.kt
+++ src/main/kotlin/org/mvnsearch/plugins/just/lang/marker/CommandLineMarkerProvider.kt
@@ -3,6 +3,7 @@ package org.mvnsearch.plugins.just.lang.marker
 import com.intellij.codeInsight.daemon.GutterName
 import com.intellij.codeInsight.daemon.LineMarkerInfo
 import com.intellij.codeInsight.daemon.LineMarkerProviderDescriptor
+import com.intellij.icons.AllIcons
 import com.intellij.openapi.editor.markup.GutterIconRenderer
 import com.intellij.openapi.util.TextRange
 import com.intellij.psi.PsiElement
@@ -13,15 +14,24 @@ import org.mvnsearch.plugins.just.lang.psi.JustTypes
 
 class CommandLineMarkerProvider : LineMarkerProviderDescriptor() {
     override fun getName(): @GutterName String {
-        return "Command mode"
+        return "Shebang"
     }
 
     override fun getLineMarkerInfo(psiElement: PsiElement): LineMarkerInfo<*>? {
         if (psiElement.elementType == JustTypes.CODE_BLOCK) {
             val elementText = psiElement.text
             val lines = elementText.trim().lines()
-            val firstLine = lines.first()
-            if (firstLine.trim().startsWith("#!")) {
+            val firstLine = lines.first().trim()
+            if (firstLine.startsWith("#!")) {
+                var icon = AllIcons.Nodes.Console
+                var hint = "Bash"
+                if (firstLine.contains("python") || firstLine.contains(" uv")) {
+                    icon = AllIcons.Language.Python
+                    hint = "Python"
+                } else if (firstLine.contains("ruby")) {
+                    icon = AllIcons.Language.Ruby
+                    hint = "Ruby"
+                }
                 val offset = elementText.indexOf(firstLine)
                 val elementStartOffset = psiElement.startOffset
                 val textRange =
@@ -29,13 +39,13 @@ class CommandLineMarkerProvider : LineMarkerProviderDescriptor() {
                 return LineMarkerInfo(
                     psiElement,
                     textRange,
-                    com.intellij.icons.AllIcons.Run.ShowIgnored,
+                    icon,
                     { _: PsiElement? ->
-                        "Quiet mode"
+                        hint
                     }, null,
                     GutterIconRenderer.Alignment.CENTER,
                     {
-                        "Quiet mode"
+                        hint
                     }
                 )
             }
