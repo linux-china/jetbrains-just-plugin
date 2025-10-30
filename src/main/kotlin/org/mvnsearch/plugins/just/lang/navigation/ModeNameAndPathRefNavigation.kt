e839e4b9d6fd8c4f2eb33c9c2fad9cc1f8d015a2
leijuan <libing.chen@gmail.com>
2025-10-30 12:00:56 +0800

    bug: fix module file search #67

--- src/main/kotlin/org/mvnsearch/plugins/just/lang/navigation/ModeNameAndPathRefNavigation.kt
+++ src/main/kotlin/org/mvnsearch/plugins/just/lang/navigation/ModeNameAndPathRefNavigation.kt
@@ -2,6 +2,7 @@ package org.mvnsearch.plugins.just.lang.navigation
 
 import com.intellij.navigation.DirectNavigationProvider
 import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiManager
 import com.intellij.psi.util.elementType
 import org.mvnsearch.plugins.just.lang.psi.JustFile
 import org.mvnsearch.plugins.just.lang.psi.JustModStatement
@@ -13,16 +14,27 @@ class ModeNameAndPathRefNavigation : DirectNavigationProvider {
     override fun getNavigationElement(element: PsiElement): PsiElement? {
         if (element.elementType == JustTypes.MOD_NAME) {
             val justFile = element.containingFile as JustFile
-            if (justFile.isPhysical) {
+            if (justFile.isPhysical && justFile.containingDirectory != null) {
                 val modeName = element.text
                 val modStatement = element.parent as JustModStatement
                 var modFilePath = "$modeName.just"
                 modStatement.modPath?.let { modePath ->
                     modFilePath = modePath.text.trim { it == '\"' || it == '\'' }
                 }
-                justFile.containingDirectory?.let { directory ->
-                    directory.findFile(modFilePath)?.let {
-                        return it
+                val justfileVirtualDir = justFile.containingDirectory!!.virtualFile
+                // If a module is named foo, just will search for the module file in foo.just,
+                // foo/mod.just, foo/justfile, and foo/.justfile.
+                val allModFiles = listOf(
+                    modFilePath,
+                    "${modeName}/mod.just",
+                    "${modeName}/justfile",
+                    "${modeName}/Justfile",
+                    "${modeName}/.justfile",
+                    "${modeName}/.Justfile"
+                )
+                for (modFile in allModFiles) {
+                    justfileVirtualDir.findFileByRelativePath(modFile)?.let {
+                        return PsiManager.getInstance(element.project).findFile(it)
                     }
                 }
             }
