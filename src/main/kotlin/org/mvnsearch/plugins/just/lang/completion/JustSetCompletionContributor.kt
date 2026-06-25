d695bfc7c6e21d9afff9fc711f34387b8861c95e
linux_china <libing.chen@gmail.com>
2026-06-25 08:59:32 +0800

    chore: add minimum-version setting

--- src/main/kotlin/org/mvnsearch/plugins/just/lang/completion/JustSetCompletionContributor.kt
+++ src/main/kotlin/org/mvnsearch/plugins/just/lang/completion/JustSetCompletionContributor.kt
@@ -1,6 +1,7 @@
 package org.mvnsearch.plugins.just.lang.completion
 
 import com.intellij.codeInsight.completion.*
+import com.intellij.codeInsight.lookup.LookupElement
 import com.intellij.codeInsight.lookup.LookupElementBuilder
 import com.intellij.patterns.PlatformPatterns
 import com.intellij.patterns.PlatformPatterns.psiElement
@@ -14,7 +15,8 @@ import org.mvnsearch.plugins.just.lang.psi.JustTypes
 class JustSetCompletionContributor : CompletionContributor() {
     init {
         extend(
-            CompletionType.BASIC, psiElement(PsiElement::class.java).withElementType(JustTypes.SETTING).withLanguage(JustLanguage),
+            CompletionType.BASIC,
+            psiElement(PsiElement::class.java).withElementType(JustTypes.SETTING).withLanguage(JustLanguage),
             object : CompletionProvider<CompletionParameters>() {
                 override fun addCompletions(
                     parameters: CompletionParameters,
@@ -23,17 +25,24 @@ class JustSetCompletionContributor : CompletionContributor() {
                 ) {
                     val element = parameters.position
                     if (element.elementType == JustTypes.SETTING) {
+                        val insertHandler: (InsertionContext, LookupElement) -> Unit =
+                            { context, _ -> context.editor.caretModel.moveToOffset(context.tailOffset - 1) }
                         result.addElement(LookupElementBuilder.create("allow-duplicate-recipes"))
                         result.addElement(LookupElementBuilder.create("allow-duplicate-variables"))
-                        result.addElement(LookupElementBuilder.create("dotenv-load := true").withPresentableText("dotenv-load"))
-                        result.addElement(LookupElementBuilder.create("dotenv-command := \"\"")
-                            .withInsertHandler { context, _ -> context.editor.caretModel.moveToOffset(context.tailOffset - 1) })
+                        result.addElement(
+                            LookupElementBuilder.create("dotenv-load := true").withPresentableText("dotenv-load")
+                        )
+                        result.addElement(
+                            LookupElementBuilder.create("dotenv-command := \"\"").withInsertHandler(insertHandler)
+                        )
                         result.addElement(LookupElementBuilder.create("dotenv-required"))
-                        result.addElement(LookupElementBuilder.create("dotenv-filename := \"\"")
-                            .withInsertHandler { context, _ -> context.editor.caretModel.moveToOffset(context.tailOffset - 1) })
+                        result.addElement(
+                            LookupElementBuilder.create("dotenv-filename := \"\"").withInsertHandler(insertHandler)
+                        )
                         result.addElement(LookupElementBuilder.create("dotenv-override := true"))
-                        result.addElement(LookupElementBuilder.create("dotenv-path := \"\"")
-                            .withInsertHandler { context, _ -> context.editor.caretModel.moveToOffset(context.tailOffset - 1) })
+                        result.addElement(
+                            LookupElementBuilder.create("dotenv-path := \"\"").withInsertHandler(insertHandler)
+                        )
                         result.addElement(LookupElementBuilder.create("ignore-comments"))
                         result.addElement(LookupElementBuilder.create("default-list := true"))
                         result.addElement(LookupElementBuilder.create("export"))
@@ -45,13 +54,23 @@ class JustSetCompletionContributor : CompletionContributor() {
                         result.addElement(LookupElementBuilder.create("script-interpreter"))
                         result.addElement(LookupElementBuilder.create("default-script := true"))
                         result.addElement(LookupElementBuilder.create("no-cd"))
-                        result.addElement(LookupElementBuilder.create("tempdir := \"\"")
-                            .withInsertHandler { context, _ -> context.editor.caretModel.moveToOffset(context.tailOffset - 1) })
-                        result.addElement(LookupElementBuilder.create("working-directory := \"\"")
-                            .withInsertHandler { context, _ -> context.editor.caretModel.moveToOffset(context.tailOffset - 1) })
+                        result.addElement(
+                            LookupElementBuilder.create("tempdir := \"\"").withInsertHandler(insertHandler)
+                        )
+                        result.addElement(
+                            LookupElementBuilder.create("working-directory := \"\"").withInsertHandler(insertHandler)
+                        )
+                        result.addElement(
+                            LookupElementBuilder.create("minimum-version := \"\"").withInsertHandler(insertHandler)
+                        )
                         result.addElement(LookupElementBuilder.create("positional-arguments"))
-                        result.addElement(LookupElementBuilder.create("shell := [\"bash\", \"-c\"]").withPresentableText("shell"))
-                        result.addElement(LookupElementBuilder.create("windows-shell := [\"pwsh.exe\", \"-NoLogo\",\"-Command\"]").withPresentableText("windows-shell"))
+                        result.addElement(
+                            LookupElementBuilder.create("shell := [\"bash\", \"-c\"]").withPresentableText("shell")
+                        )
+                        result.addElement(
+                            LookupElementBuilder.create("windows-shell := [\"pwsh.exe\", \"-NoLogo\",\"-Command\"]")
+                                .withPresentableText("windows-shell")
+                        )
                         result.addElement(LookupElementBuilder.create("windows-powershell := true"))
                     }
                 }
